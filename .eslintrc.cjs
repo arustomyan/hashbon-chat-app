aa7f76fb31c983611d9934223d0ec4d0f1923404
Вячеслав <90838956+arustomyan@users.noreply.github.com>
2023-08-16 17:25:07 +0300

    feat: created layout chatpage

---
+++ .eslintrc.cjs
@@ -0,0 +1,47 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:eslint-plugin-import/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "airbnb",
+    "airbnb/hooks",
+    "plugin:react/jsx-runtime",
+    "prettier",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "prettier"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      {
+        allowConstantExport: true,
+      },
+    ],
+    "import/no-unresolved": "off",
+    "import/no-absolute-path": "off",
+    "no-shadow": "off",
+    "react/self-closing-comp": "off",
+    "prettier/prettier": "error",
+    "react/function-component-definition": [
+      2,
+      {
+        namedComponents: "arrow-function",
+        unnamedComponents: "arrow-function",
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ["*.tsx"],
+      rules: {
+        "react/jsx-filename-extension": "off",
+      },
+    },
+  ],
+};
